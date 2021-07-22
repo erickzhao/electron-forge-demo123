efe64f7546934020f86337390cf9c0d8df509e26
Mark Lee <malept@users.noreply.github.com>
2021-07-21 17:03:51 -0700

    feat!: Electron Forge 6 (#3)
    * Convert to Forge 6
    
    * Update to latest Forge
    
    * Add branch to CI
    
    * Try to fix up the config
    
    * Missed a null
    
    * Add electron itself
    
    * Use Ubuntu 18.04
    
    * Re-add zip for darwin
    
    * source-map-support isn't installed on Windows for some reason
    
    * Drop squirrel
    
    * Just snap and zip
    
    * More snap infra
    
    * Add GitHub Actions for Linux
    
    * Temporarily add branch to GitHub Actions
    
    * Add snap config
    
    * before_install only for Linux
    
    * AppVeyor supports macOS/Linux
    
    * Run make/publish
    
    * Set PATH in PS & install Snapcraft
    
    * Print PATH
    
    * Debug env
    
    * More debug
    
    * Fix repo tag checks
    
    * Remove env
    
    * Fix Windows cache location
    
    * All OSes
    
    * Fix env matrix
    
    * Consolidate
    
    * Install snapcraft
    
    * Fix GHA syntax
    
    * Use yarn & convert GHA to stages
    
    * Use include correctly
    
    * Array?
    
    * Try another thing
    
    * Split up artifacts & try to read the MAKER env variable
    
    * Fix typo
    
    * Update lockfile
    
    * Fix download path
    
    * Debug script
    
    * Always run the distributable script with bash
    
    * Show out dir
    
    * show distributable dir
    
    * Re-add deb/rpm
    
    * Add artifacts in AppVeyor
    
    * Add support for mas & upload distributable artifacts to GHA
    
    * Better glob for AppVeyor artifacts
    
    * Install Linux deps on AppVeyor
    
    * Fix typo
    
    * Fix typo
    
    * Install Yarn on macOS AppVeyor
    
    * Set up platform for make
    
    * Stop caching for macOS because it's 1.2GB (vs 20MB for Linux/Windows)
    
    * Missed the bundle step
    
    * Fix typos
    
    * Add dmg/pkg
    
    * Sign darwin/mas builds
    
    * Debug macOS cert
    
    * Try to set the identity explicitly
    
    * Debug electron-osx-sign
    
    * Set identity for pkg
    
    * Disable gatekeeper-assess
    
    * Temporarily disable pkg builds
    
    * Remove osx-sign debug
    
    * GitHub Actions: add lint stage
    
    * Travis CI: use build stages
    
    * Travis CI: disable before_install for lint
    
    * Travis CI: disable test stage
    
    * Travis CI: move DEBUG var
    
    * Travis CI: YARN_GPG only for Windows
    
    * Travis CI: package & make in the same stage due to no artifact storage
    
    * Add flatpak
    
    * Upgrade all dependencies
    
    * Fix permissions on flatpak dependencies install script
    
    * Allow all platforms to make zips
    
    * Make linux zips too
    
    * Work around outdated apt repo keys on GitHub Actions
    
    * Ensure dpkg/fakeroot/rpm are installed
    
    * build(deps-dev): upgrade electron to ^9.0.5
    
    * build(deps-dev): upgrade Electron Forge to 6.0.0-beta.52
    
    * build: upgrade transient dependencies
    
    * build: install Linux maker dependencies conditionally
    
    * build(travis): fix os typos
    
    * build: remove always-run apt command
    
    * build: debug platform setup script
    
    * build(github-actions): set MAKER when setting up platform dependencies
    
    * fix: set recommended security settings
    
    * build(deps): upgrade transitive dependencies
    
    * build(ci): add Forge Actions, Windows certificates, & Squirrel installers (#2)
    
    * build(deps-dev): upgrade electron to ^10.1.2
    
    * chore: disable pkg
    
    * build(deps): upgrade transitive dependencies
    
    * chore: upgrade Electron to ^11
    
    * build(deps): upgrade transitive dependencies
    
    * Remove obsolete eslint config
    
    * build: upgrade transitive dependencies
    
    * ci: bazel doesn't exist anymore in the GitHub Actions runner it seems
    
    * ci: opensuse apt sources don't exist either
    
    * chore: upgrade dependencies
    
    * ci: build multiple arches
    
    * ci: be more granular about OS
    
    * ci: fix darwin/dmg
    
    * ci: temporarily disable arm64 flatpak/snap since they can't be cross-compiled
    
    * build(deps): upgrade dependencies
    
    * build(deps-dev): upgrade to Electron 13
    
    * ci: use yarn cache on all OSes
    
    * ci: use Node 12
    
    * ci: tar bundled apps to preserve permissions & symlinks
    
    * build(deps): upgrade dependencies
    
    * ci: platform dependencies script is bash
    
    * ci: use builtin caching in setup-node
    
    * ci(github): distributable-action handles platform dependencies
    
    * build(deps-dev): upgrade to Forge beta 58
    
    * chore: update transitive dependencies
    
    * build(deps): upgrade transitive dependencies
    
    * master -> main
    
    * ci(travis): build arm64 (#4)
    
    * ci(appveyor): x64 only
    
    * ci(appveyor): use Node.js built for x64

---
+++ ci/platform_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -xe
+
+case "$(uname -s)" in
+    "Linux")
+        if [[ "$MAKER" = "deb" || -n "$APPVEYOR" ]]; then
+          sudo apt-get install --yes --no-install-recommends fakeroot dpkg
+        fi
+        if [[ "$MAKER" = "rpm" || -n "$APPVEYOR" ]]; then
+          sudo apt-get install --yes --no-install-recommends rpm
+        fi
+        if [[ "$MAKER" = "flatpak" || -n "$APPVEYOR" ]]; then
+          "$(dirname $0)"/install_flatpak_dependencies.sh
+        fi
+        if [[ "$MAKER" = "snap" || -n "$APPVEYOR" ]]; then
+          "$(dirname $0)"/install_snap_dependencies.sh
+        fi
+        ;;
+    "Darwin")
+        "$(dirname $0)"/codesign/import-testing-cert-ci.sh
+        ;;
+esac
